5c743e94c920d01124b3e82593d3ab6d70c9e7e1
Lee Changmoo <lcm8543@gmail.com>
2021-11-11 22:07:50 +0900

    wireframe update

--- client/src/components/pages/my_page.js
+++ client/src/components/pages/my_page.js
@@ -5,9 +5,63 @@ class my_page extends Component {
   render() {
     return (
       <>
-        <h1>path='/my_page'</h1>
-        <h3>my_page</h3>
-        <Link to={"/main"}>main</Link>
+        <div className="mypage_box">
+          <div className="mypage_rowSpace">
+            <h1>path='/my_page'</h1>
+            <h3>my_page</h3>
+            <Link to={"/main"}>main</Link>
+          </div>
+          <div className="mypage_middleSpace">
+            <div className="mypage_middleSpace_high">
+
+            </div>
+
+            <div className="mypage_middleSpace_middle">
+
+              <div className="mypage_middleSpace_myRegisteredInfo">
+                <div className="mypage_lost_pet_info_high">
+                  <div className="mypage_pet_name">
+                    이름
+                  </div>
+                  <div className="mypage_pet_lost_day">
+                    잃어버린날짜
+                  </div>
+                  <div className="mypage_pet_age">
+                    나이
+                  </div>
+                  <div className="mypage_pet_location">
+                    지역명
+                  </div>
+                  <div className="mypage_pet_info_high_space">
+                    <button className="mypage_middleSpace_myRegisteredInfo_exit">
+                      x
+                    </button>
+                  </div>
+                </div>
+                <div className="mypage_lost_pet_info_low">
+                  <div className="mypage_pet_image">
+                    <image>펫 이미지</image>
+                  </div>
+                  <div className="mypage_pet_describe">
+                    피드내용
+                  </div>
+                  <div className="mypage_pet_toggle_and_revise">
+                    <div>toggle</div>
+                    <button>수정하기</button>
+                  </div>
+                </div>
+              </div>
+
+            </div>
+
+            <div className="mypage_middleSpace_low">
+              <button className="userEdit_button">회원정보 수정하기</button>
+            </div>
+          </div>
+          <div className="mypage_rowSpace">
+
+          </div>
+        </div>
       </>
     );
   }
