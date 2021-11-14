3797ac9d9c5cf6e162cb0ef667363dadfb07510c
gitkrak <dpemdnjem23@gamail.com>
2021-11-14 21:19:40 +0900

    feat

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,13 +1,4 @@
-<<<<<<< HEAD
-const router = require("express").Router();
-const controller = require("../controllers/auth");
 
-router.post("/signin", controller.signinController);
-router.post("/signup", controller.signupController);
-router.post("/googlesignin", controller.googlesigninController);
-
-module.exports = router;
-=======
 const express = require('express')
 const router = express.Router()
 const controller =require('../controllers/auth') //controller auth.js module 객체 사용
@@ -19,4 +10,4 @@ router.post('/signup',controller.signupControl)
 // router.post('/googlesignin')
 //소셜로그인(구글) /auth/googlesignin
 module.exports = router
->>>>>>> b22fe6f2dd68442a707479d3da1c5c7b010bbdf0
+
--- server/routes/intro.js
+++ server/routes/intro.js
@@ -1,18 +1,7 @@
-<<<<<<< HEAD
+
 const router = require("express").Router();
 const controller = require("../controllers/intro");
 
 router.get("/", controller.introController);
 
 module.exports = router;
-=======
-const express = require('express')
-
-const router = express.Router()
-
-// const controller = require('../controller')
-
-
-// router.get('/intro',controller.intro)
-module.exports = router
->>>>>>> b22fe6f2dd68442a707479d3da1c5c7b010bbdf0
